1b1b18dd9dd5e13cef7f68bea0beb3366199dcba
BarminaSK <barminask@mail.ru>
2025-11-12 01:08:54 +0500

    Task7

---
+++ Tyuiu.BarminaSK.Sprint4.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BarminaSK.Sprint4.Task7.V5.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint4.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int n = 3;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "246813579";
+            DataService ds = new DataService();
+            int res = ds.Calculate(n, m, str);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint4.Task7.V5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.BarminaSK.Sprint4.Task7.V5.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint4.Task7.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "246813579";
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Бармина С.К. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Задание #7                                                               *");
+            Console.WriteLine("* Вариант #5                                                             *");
+            Console.WriteLine("* Выполнила: Бармина София Константиновна | ПИНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(n, m, str);
+
+            Console.WriteLine("Количество четных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
